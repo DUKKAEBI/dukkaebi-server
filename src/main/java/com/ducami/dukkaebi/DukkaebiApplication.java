d1a16d1126f168bb5ae3471235317223fda6138f
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-11-12 14:23:42 +0900

    first commit

---
+++ src/main/java/com/ducami/dukkaebi/DukkaebiApplication.java
@@ -0,0 +1,13 @@
+package com.ducami.dukkaebi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DukkaebiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DukkaebiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ducami/dukkaebi/DukkaebiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ducami.dukkaebi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DukkaebiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
