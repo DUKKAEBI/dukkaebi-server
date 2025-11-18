45eeb1223762906ccbb4af82d93ace4af32221a2
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-11-18 16:35:26 +0900

    feat :: contest setting

---
+++ src/main/java/com/ducami/dukkaebi/domain/contest/domain/repo/ContestJpaRepo.java
@@ -0,0 +1,9 @@
+package com.ducami.dukkaebi.domain.contest.domain.repo;
+
+import com.ducami.dukkaebi.domain.contest.domain.Contest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContestJpaRepo extends JpaRepository<Contest, Long> {
+}
