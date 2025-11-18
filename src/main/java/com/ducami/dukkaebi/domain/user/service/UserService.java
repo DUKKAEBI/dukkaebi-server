5f67ee132fb69c68354d36a487371602852a5b5d
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-11-18 23:36:56 +0900

    feat :: score

--- src/main/java/com/ducami/dukkaebi/domain/user/service/UserService.java
+++ src/main/java/com/ducami/dukkaebi/domain/user/service/UserService.java
@@ -2,7 +2,7 @@ package com.ducami.dukkaebi.domain.user.service;
 
 import com.ducami.dukkaebi.domain.user.domain.User;
 import com.ducami.dukkaebi.domain.user.domain.repo.UserJpaRepo;
-import com.ducami.dukkaebi.domain.user.error.UserError;
+import com.ducami.dukkaebi.domain.user.error.UserErrorCode;
 import com.ducami.dukkaebi.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,11 +10,10 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-
     private final UserJpaRepo userJpaRepo;
 
     public User findByUserId(Long userId) {
         return userJpaRepo.findById(userId)
-                .orElseThrow(() -> new CustomException(UserError.USER_NOT_FOUND));
+                .orElseThrow(() -> new CustomException(UserErrorCode.USER_NOT_FOUND));
     }
 }
