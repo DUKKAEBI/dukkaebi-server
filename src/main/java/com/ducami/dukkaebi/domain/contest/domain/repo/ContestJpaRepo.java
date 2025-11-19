9604d19b9587bae480eb25020ef2ba20c29905df
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-11-19 19:32:12 +0900

    feat :: contest list

--- src/main/java/com/ducami/dukkaebi/domain/contest/domain/repo/ContestJpaRepo.java
+++ src/main/java/com/ducami/dukkaebi/domain/contest/domain/repo/ContestJpaRepo.java
@@ -4,7 +4,10 @@ import com.ducami.dukkaebi.domain.contest.domain.Contest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ContestJpaRepo extends JpaRepository<Contest, String> {
     boolean existsByTitle(String title);
+    List<Contest> findAllByOrderByEndDateAsc();
 }
