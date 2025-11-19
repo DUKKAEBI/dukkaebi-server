9439018121baf66ecc231b753ee64ca50bda5653
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-11-19 14:22:53 +0900

    .

--- src/main/java/com/ducami/dukkaebi/domain/contest/presentation/ContestController.java
+++ src/main/java/com/ducami/dukkaebi/domain/contest/presentation/ContestController.java
@@ -1,6 +1,9 @@
 package com.ducami.dukkaebi.domain.contest.presentation;
 
+import com.ducami.dukkaebi.domain.contest.presentation.dto.request.ContestReq;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/contest")
 public class ContestController {
 
+    @PostMapping("/create")
+    public void createContest(@RequestBody ContestReq req) {
+        return;
+    }
 }
