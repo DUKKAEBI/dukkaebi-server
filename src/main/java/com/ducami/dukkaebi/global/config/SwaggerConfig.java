d61073fcf4cdb90d6fb3b8d047830e9d0b31904f
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-11-20 21:01:48 +0900

    chore :: swagger

--- src/main/java/com/ducami/dukkaebi/global/config/SwaggerConfig.java
+++ src/main/java/com/ducami/dukkaebi/global/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,8 +17,10 @@ public class SwaggerConfig {
                 .info(
                         new Info().title("Dukkaebi API")
                                 .description("Dukkaebi API")
-                                .version("1.0.0")
+                                .version("1.0.5")
                 )
+                .addServersItem(new Server().url("https://dukkaebi.o-r.kr"))
+                .addServersItem(new Server().url("http://localhost:8080"))
                 .addSecurityItem(new SecurityRequirement().addList("Authorization"))
                 .components(
                         new Components()
