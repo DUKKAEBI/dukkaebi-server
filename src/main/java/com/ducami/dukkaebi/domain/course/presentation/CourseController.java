18bd9049332a091763b64013c2d8bbc4b5a54ede
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-12-15 11:49:07 +0900

    feat :: student course

--- src/main/java/com/ducami/dukkaebi/domain/course/presentation/CourseController.java
+++ src/main/java/com/ducami/dukkaebi/domain/course/presentation/CourseController.java
@@ -3,6 +3,7 @@ package com.ducami.dukkaebi.domain.course.presentation;
 import com.ducami.dukkaebi.domain.course.presentation.dto.response.CourseDetailRes;
 import com.ducami.dukkaebi.domain.course.presentation.dto.response.CourseListRes;
 import com.ducami.dukkaebi.domain.course.usecase.CourseUseCase;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,11 +19,13 @@ public class CourseController {
     private final CourseUseCase courseUseCase;
 
     @GetMapping("/list")
+    @Operation(summary = "코스 리스트 조회")
     public List<CourseListRes> getCourseList() {
         return courseUseCase.getCourseList();
     }
 
     @GetMapping("/{courseId}")
+    @Operation(summary = "코스 상세 조회")
     public CourseDetailRes getCourseDetail(@PathVariable("courseId") Long courseId) {
         return courseUseCase.getCourseDetail(courseId);
     }
