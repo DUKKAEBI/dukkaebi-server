058832aee2530ffe9e613e6796020b9cb66099a3
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-12-15 10:59:50 +0900

    feat :: admin course

--- src/main/java/com/ducami/dukkaebi/domain/course/usecase/CourseAdminUseCase.java
+++ src/main/java/com/ducami/dukkaebi/domain/course/usecase/CourseAdminUseCase.java
@@ -1,5 +1,6 @@
 package com.ducami.dukkaebi.domain.course.usecase;
 
+import com.ducami.dukkaebi.domain.course.domain.Course;
 import com.ducami.dukkaebi.domain.course.domain.repo.CourseJpaRepo;
 import com.ducami.dukkaebi.domain.course.error.CourseErrorCode;
 import com.ducami.dukkaebi.domain.course.presentation.dto.request.CourseReq;
@@ -23,5 +24,29 @@ public class CourseAdminUseCase {
         courseJpaRepo.save(CourseReq.fromReq(req));
 
         return Response.created("코스가 성공적으로 생성되었습니다.");
+    }
+
+    @Transactional
+    public Response updateCourse(Long courseId, CourseReq req) {
+        Course course = courseJpaRepo.findById(courseId)
+                .orElseThrow(() -> new CustomException(CourseErrorCode.COURSE_NOT_FOUND));
+
+        if (!course.getTitle().equals(req.title()) && courseJpaRepo.existsByTitle(req.title())) {
+            throw new CustomException(CourseErrorCode.TITLE_ALREADY);
+        }
+
+        course.updateCourse(req.title(), req.description(), req.keywords(), req.level());
+
+        return Response.ok("코스가 성공적으로 수정되었습니다.");
+    }
+
+    @Transactional
+    public Response deleteCourse(Long courseId) {
+        Course course = courseJpaRepo.findById(courseId)
+                .orElseThrow(() -> new CustomException(CourseErrorCode.COURSE_NOT_FOUND));
+
+        courseJpaRepo.delete(course);
+
+        return Response.ok("코스가 성공적으로 삭제되었습니다.");
     }
 }
