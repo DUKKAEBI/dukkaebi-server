be50925cfd22753c4765905232fd4621cdc211be
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-12-17 20:22:16 +0900

    fix :: 학생 코스 조회

--- src/main/java/com/ducami/dukkaebi/domain/course/presentation/dto/response/CourseStudentItemRes.java
+++ src/main/java/com/ducami/dukkaebi/domain/course/presentation/dto/response/CourseStudentItemRes.java
@@ -2,10 +2,12 @@ package com.ducami.dukkaebi.domain.course.presentation.dto.response;
 
 import com.ducami.dukkaebi.domain.course.domain.Course;
 import com.ducami.dukkaebi.domain.course.domain.enums.CourseStatus;
+import com.ducami.dukkaebi.domain.course.domain.enums.LevelType;
 
 public record CourseStudentItemRes(
         Long courseId,
         String title,
+        LevelType level,
         int progressPercent,
         CourseStatus status
 ) {
@@ -13,6 +15,7 @@ public record CourseStudentItemRes(
         return new CourseStudentItemRes(
                 course.getId(),
                 course.getTitle(),
+                course.getLevel(),
                 progressPercent,
                 status
         );
