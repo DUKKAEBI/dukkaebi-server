49ef53b5ca26f7e5f0e8cdf3cf23ac183871910d
tlsalsco41122 <tlsalsco41122@gmail.com>
2026-01-16 23:00:29 +0900

    refactor

--- src/main/java/com/ducami/dukkaebi/domain/user/presentation/controller/UserController.java
+++ src/main/java/com/ducami/dukkaebi/domain/user/presentation/controller/UserController.java
@@ -12,13 +12,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
